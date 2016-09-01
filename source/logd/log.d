7d0dd9a1abc6ed4384f633b9c47d397f8dfa28d5
Amaranthos <joshua.hodkinson.42@gmail.com>
2016-09-01 19:48:31 +1000

    Moved log functions from global scope into static class Logd

--- source/logd/log.d
+++ source/logd/log.d
@@ -1,3 +1,4 @@
+
 module logd.log;
 
 import std.stdio;
@@ -6,13 +7,12 @@ import std.string;
 import std.datetime;
 import std.format;
 
-private {
-	__gshared File log;
-	__gshared bool isLogging = true;
-}
+static class Logd {
+	
+	static private __gshared File log;
+	static private __gshared bool isLogging = true;
 
-public {
-	enum Level {
+	public enum Level {
 		success = "success",
 		event = "event",
 		warning = "warning",
@@ -32,63 +32,66 @@ public {
 		log.close();
 	}
 
-	void SetLogFilename(string filename) {
+	static public void SetLogFilename(string filename) {
 		log.close();
 		log.open(filename, "w");
 		WriteHtmlHeader();
 	}
 
-	string GetFilename() {
+	static public string GetFilename() {
 		return log.name;
 	}
 
-	void Log()() {
+	static public void Write()() {
 		writeln();
-		if(isLogging) log.writeln();
+		if(isLogging) 
+			log.writeln();
 	}
 
-	void Log(Level, T...)(Level level, T t) {
+	static public void Write(Level, T...)(Level level, T t) {
 		writeln(t);
-		if(isLogging) log.writeln("<p class=", level, ">", CurrentTime(),t,"</p>");
+		if(isLogging) 
+			log.writeln("<p class=", level, ">", CurrentTime(),t,"</p>");
 	}
 
-	void LogTag(Level, Fmt, T...)(Level level, Fmt fmt, T t){
+	static public void WriteWithTag(Level, string, T...)(Level level, string fmt, T t){
 		writeln(t);
-		if(isLogging) log.writeln("<",fmt," class=", level, ">",t,"</",fmt,">");
+		if(isLogging) 
+			log.writeln("<",fmt," class=", level, ">",t,"</",fmt,">");
 	}
 
-	void EnableLogging(bool logging) {
+	static public void EnableLogging(bool logging) {
 		isLogging = logging;
 	}
 
-	bool IsLogging() {
+	static public bool IsLogging() {
 		return isLogging;
 	}
 
-	void Flush() {
+	static public void Flush() {
 		std.stdio.stdout.flush();
 		log.flush();
 	}	
 
-	string CurrentTime() {
+	static public string CurrentTime() {
 		auto time = Clock.currTime;
 		return format("(%s/%s/%s %s:%s:%s) - ", time.day, cast(int)time.month, time.year, time.hour, time.minute, time.second);
 	}
-}
 
-private void WriteHtmlHeader(){
-	log.writeln("<!DOCTYPE html>");
-	log.writeln("<html>");
-	log.writeln("<head>");
-	log.writeln("<title>Log</title>");
-	log.writeln("<style>");
-	log.writeln("body{background-color: black;} p{margin-top: 5px; margin-bottom: 5px;} .success {color: lime;} .warning {color: orange;} .error {color: red;} .event {color: yellow;} .user{color: aqua;} .update {color: white;}");
-	log.writeln("</style>");
-	log.writeln("</head>");
-	log.writeln("<body>");	
-}
+	static private void WriteHtmlHeader(){
+		log.writeln("<!DOCTYPE html>");
+		log.writeln("<html>");
+		log.writeln("<head>");
+		log.writeln("<title>Log</title>");
+		log.writeln("<style>");
+		log.writeln("body{background-color: black;} p{margin-top: 5px; margin-bottom: 5px;} .success {color: lime;} .warning {color: orange;} .error {color: red;} .event {color: yellow;} .user{color: aqua;} .update {color: white;}");
+		log.writeln("</style>");
+		log.writeln("</head>");
+		log.writeln("<body>");	
+	}
 
-private void WriteHtmlFooter() {
-	log.writeln("</body>");
-	log.writeln("</html>");
+	static private void WriteHtmlFooter() {
+		log.writeln("</body>");
+		log.writeln("</html>");
+	}	
 }
